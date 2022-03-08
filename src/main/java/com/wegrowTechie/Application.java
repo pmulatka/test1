8b74e1ca3765f0e4ee25072f7795e7541ff782ac
Pooja Mulatkar <shimpi.pooja@gmail.com>
2022-03-08 16:42:42 +0530

    first commit

---
+++ src/main/java/com/wegrowTechie/Application.java
@@ -0,0 +1,22 @@
+package com.wegrowTechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping
+	public String message() {
+		
+		return "Hi I friends, am there for help you ";
+	}
+
+}
